49025738609c7f2e26178965e184e36ca386b695
jess-nl <jnoriegalessard@gmail.com>
2024-09-12 16:41:28 -0400

    feat: implement neetcode exercises 21, 206

---
+++ NeetCode/LinkedList/206_ReverseLinkedList.cs
@@ -0,0 +1,23 @@
+﻿using Practice.Helper.Nodes;
+
+namespace Practice.NeetCode.LinkedList
+{
+    public class ReverseLinkedList
+    {
+        public Node ReverseList(Node head)
+        {
+            Node previous = null;
+            var current = head;
+
+            while (current != null)
+            {
+                var next = current.next;
+                current.next = previous;
+                previous = current;
+                current = next;
+            }
+
+            return previous;
+        }
+    }
+}
